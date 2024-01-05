3c39b884d5bff96506583ec7271127d5da887eb5
LordMcSniff <lordmcsniff@tu-berlin.de>
2024-01-05 22:39:28 +0100

    slain the sphinx

--- 07ex.c
+++ 07ex.c
@@ -10,10 +10,11 @@ Wir empfehlen, mit möglichst streng eingestelltem valgrind zu testen, denn so t
 cc -std=c11 -g -Wall -Werror 07ex_test.c -o 07ex_test.o -lm && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./07ex_test.o
 */
 
-#include "sphinx.h"
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+#include "sphinx.h"
 
 /*
 Aufgabe 1:
@@ -28,6 +29,33 @@ Geben Sie zurück, ob das Array der Sphinx die Zahl 12345 enthält. Um den Wert 
 zu erfragen, rufen Sie `sphinx_ask(s, i)` auf. Aber Achtung: Wenn Sie diese Funktion mehr als
 1 + log2(n) (aufgerundet) mal aufrufen, stürzt das Programm fehlerhaft ab.
 */
+#define VALUE 12345
+
 bool descending_sphinx(Sphinx *s, size_t n) {
+    printf(" > \n");
+    size_t left = 0;
+    size_t right = n - 1;
+    size_t index = 0;
+
+    if (n < 1)
+        return false;
+
+    while (left <= right && right != UINT64_MAX && left != n) {
+        index = (left + right) / 2;
+        uint16_t value = sphinx_ask(s, index);
+        // printf("  %lu,  %lu,  %lu  - %hu \n", left, right, index, value);
+
+        if (value == VALUE)
+            return true;
+
+        if (left == right)
+            break;
+
+        if (value > VALUE)
+            left = index + 1;
+        else if (value < VALUE)
+            right = index - 1;
+    }
+
     return false;
 }
