884c2c86639622b8421d4b2c973cc5abd41ca845
LordMcSniff <lordmcsniff@tu-berlin.de>
2024-01-09 20:35:00 +0100

    task completed, dunno if in O(n log(count) ) though

--- 08ex.c
+++ 08ex.c
@@ -10,9 +10,9 @@ Wir empfehlen, mit möglichst streng eingestelltem valgrind zu testen, denn so t
 cc -std=c11 -g -Wall -Werror 08ex_test.c -o 08ex_test.o -lm && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./08ex_test.o
 */
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
 /* Ein Array zusammen mit seiner Länge. */
@@ -37,12 +37,18 @@ Tipp: Ja, es ist wirklich so banal wie es klingt. Das haben base-cases bei divid
 Algorithmen häufig so an sich.
 */
 ArrayWithLength base_case(ArrayWithLength arr) {
-    ArrayWithLength ret;
+    uint16_t *tmp = calloc(arr.len, sizeof(uint16_t));
 
-    ret.arr = (uint16_t *) malloc(sizeof(uint16_t) * 0);
-    ret.len = 0;
+    // once again a genius move from you guys to ban memcpy
+    // memcpy(ret.arr, arr.arr, sizeof(uint16_t) * arr.len);
 
-    return ret;
+    for (size_t i = 0; i < arr.len; i++) {
+        tmp[i] = arr.arr[i];
+    }
+
+    arr.arr = tmp;
+
+    return arr;
 }
 
 /*
@@ -53,11 +59,48 @@ geben Sie ein absteigend sortiertes Array zurück,
 welches die Elemente beider Eingabearrays enthält. Die Funktion soll in O(len1 + len2) laufen.
 Der Speicher für die Arrayelemente im Rückgabewert soll eigens mit malloc allokiert werden.
 */
+size_t min(size_t a, size_t b) {
+    return a < b ? a : b;
+}
+size_t minb(size_t a, size_t b) {
+    return a > b;
+}
+
 ArrayWithLength combine(ArrayWithLength arr1, ArrayWithLength arr2) {
-    ArrayWithLength ret;
-    
-    ret.arr = (uint16_t *) malloc(sizeof(uint16_t) * 0);
-    ret.len = 0;
+    if (arr1.len == 0)
+        return base_case(arr2);
+    if (arr2.len == 0)
+        return base_case(arr1);
+
+    ArrayWithLength ret = {.arr = NULL, .len = 0};
+    size_t total_len = arr1.len + arr2.len;
+
+    ret.arr = calloc(total_len, sizeof(uint16_t));
+
+    size_t Index_A = 0, Index_B = 0;
+    uint16_t value_A = arr1.arr[0], value_B = arr2.arr[0];
+
+    while (Index_A + Index_B < total_len) {
+        if (value_A > value_B) {
+            ret.arr[ret.len] = value_A;
+            Index_A++;
+
+            if (Index_A >= arr1.len)
+                value_A = 0;
+            else
+                value_A = arr1.arr[Index_A];
+
+        } else {
+            ret.arr[ret.len] = value_B;
+            Index_B++;
+
+            if (Index_B >= arr2.len)
+                value_B = 0;
+            else
+                value_B = arr2.arr[Index_B];
+        }
+        ret.len++;
+    }
 
     return ret;
 }
@@ -71,11 +114,76 @@ Gesamtzahl an Elementen in allen Arrays ist. Dazu müssen sie clever dive-and-co
 
 Der Speicher für die Arrayelemente im Rückgabewert soll eigens mit malloc allokiert werden.
 */
-ArrayWithLength merge_k(ArrayWithLength *arrs, size_t count) {
-    ArrayWithLength ret;
-    
-    ret.arr = (uint16_t *) malloc(sizeof(uint16_t) * 0);
-    ret.len = 0;
 
+/*
+returns the index of the maximum value in the array.
+
+returns UINT16_MAX (-1) if not found, eg. array is empty.
+*/
+size_t max_index(uint16_t *arr, size_t len) {
+    if (len == 0)
+        return UINT16_MAX;
+
+    size_t index = 0;
+    for (size_t i = 0; i < len; i++) {
+        if (arr[i] > arr[index])
+            index = i;
+    }
+    return index;
+}
+
+ArrayWithLength merge_k(ArrayWithLength *arrs, size_t count) {
+    size_t total_len = 0;
+
+    //! 1. Step filter out the empty arrays and sum up length
+    for (size_t i = 0; i <= count - 1; i++) {
+        // while in the loop might as well keep a running total of the length instead of haveing a second one later
+        total_len += arrs[i].len;
+
+        if (arrs[i].len == 0) {
+            // swap, note order of the inputed array will not be preserved,
+            ArrayWithLength tmp = arrs[i];
+            arrs[i] = arrs[count - 1];
+            arrs[count - 1] = tmp;
+
+            count--;
+            i--;
+        }
+    }
+
+    //! 2. Step Handle edge cases
+    if (count == 0)
+        return (ArrayWithLength){.arr = NULL, .len = 0};
+    if (count == 1)
+        return base_case(arrs[0]);
+
+    //! 3. Step setup/ alloc the necessary arrays
+    ArrayWithLength ret = {.arr = NULL, .len = 0};
+    ret.arr = calloc(total_len, sizeof(uint16_t));
+
+    size_t *Indices = calloc(count, sizeof(size_t));
+    uint16_t *values = calloc(count, sizeof(uint16_t));
+    // fill the values with the first values
+    for (size_t i = 0; i < count; i++)
+        values[i] = arrs[i].arr[0];
+
+    //! 4. Step merge the arrays
+    // // TODO: loop condition
+    while (ret.len < total_len) {
+        size_t I = max_index(values, count);
+
+        ret.arr[ret.len] = values[I];
+        Indices[I]++;
+
+        if (Indices[I] >= arrs[I].len)
+            values[I] = 0;
+        else
+            values[I] = arrs[I].arr[Indices[I]];
+
+        ret.len++;
+    }
+
+    free(values);
+    free(Indices);
     return ret;
 }
