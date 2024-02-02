cc82759286228afd3c152481626049727cf93d4b
LordMcSniff <lordmcsniff@tu-berlin.de>
2024-02-02 15:20:58 +0100

    completed task

--- 10ex.c
+++ 10ex.c
@@ -10,13 +10,13 @@ Wir empfehlen, mit möglichst streng eingestelltem valgrind zu testen, denn so t
 cc -std=c11 -g -Wall -Werror 10ex_test.c -o 10ex_test.o -lm && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./10ex_test.o
 */
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
-#include "tree.h"
 #include "array.h"
+#include "tree.h"
 
 /*
 Aufgabe 1:
@@ -30,8 +30,33 @@ an die Testfälle anpassen. Aber wenn Sie der Intuition der Vorlesungsfolien fol
 
 Werte kommen in `t` niemals mehrfach vor.
 */
-void heapify_tree(TreeNode *t) {
-    
+
+void heapify_tree(TreeNode *t)
+{
+    //! check if reached the end of the tree
+    if (t->left == NULL)
+        return;
+
+    //! check if min heap
+
+    // check parent against lc
+    TreeNode *T = (t->item < t->left->item) ? t : t->left;
+
+    // if the rc exists check against it as well
+    if (t->right)
+        T = (T->item < t->right->item) ? T : t->right;
+
+    // break if min heap satisfied
+    if (T == t)
+        return;
+
+    //! swap
+    uint16_t tmp = T->item;
+    T->item = t->item;
+    t->item = tmp;
+
+    // continue with swaped child
+    heapify_tree(T);
 }
 
 /*
@@ -45,6 +70,30 @@ Oder knapper: Implementieren Sie den heapify Algorithmus aus der Vorlesung, aber
 
 Werte kommen im Array niemals mehrfach vor.
 */
-void heapify_array(Array arr, size_t i) {
+void heapify_array(Array arr, size_t i)
+{
+    //! check if reached the end of the tree
+    if (i * 2 + 1 >= arr.len)
+        return;
 
+    //! check if min heap
+
+    // check parent against lc
+    size_t I = (arr.arr[i] < arr.arr[i * 2 + 1]) ? i : i * 2 + 1;
+
+    // if the rc exists check against it as well
+    if (i * 2 + 2 < arr.len)
+        I = (arr.arr[I] < arr.arr[i * 2 + 2]) ? I : i * 2 + 2;
+
+    // break if min heap satisfied
+    if (I == i)
+        return;
+
+    //! swap
+    uint16_t tmp = arr.arr[I];
+    arr.arr[I] = arr.arr[i];
+    arr.arr[i] = tmp;
+
+    // continue with swaped child
+    heapify_array(arr, I);
 }
